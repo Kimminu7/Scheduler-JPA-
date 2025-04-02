3baa90a8bc0c67b5aa9f8f1df3bc21471e07fc85
kimminu7 <rock9024@naver.com>
2025-04-02 11:21:55 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/org/example/schedulerprojectv2/SchedulerProjectV2Application.java
@@ -0,0 +1,13 @@
+package org.example.schedulerprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjectV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjectV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedulerprojectv2/SchedulerProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedulerprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerProjectV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
